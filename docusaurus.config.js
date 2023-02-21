dcfdbb57d95d71fcfde1be841a3aca868eb66fbe
minisbett <39670899+minisbett@users.noreply.github.com>
2023-02-21 17:57:46 +0100

    Fix edit urls

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-          'https://github.com/minipadkb/minipad-wiki/',
+          'https://github.com/minipadKB/minipad-wiki/tree/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-          'https://github.com/minipadkb/minipad-wiki/',
+          'https://github.com/minipadKB/minipad-wiki/tree/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
