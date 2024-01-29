e3202dd4adf2dfb8fac4d2736dab4977111c32c9
mini <39670899+minisbett@users.noreply.github.com>
2024-01-29 23:18:13 +0100

    Switch from custom domain to github.io

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
     url: 'https://minipad.minii.moe',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/',
+    baseUrl: '/minipad-wiki/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
